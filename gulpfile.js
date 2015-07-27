ae9191e5f11206a51fcd166bd227ee7cb2fbdd82
gaopeng <rockts@sina.com>
2015-07-28 02:03:02 +0800

    更新中

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 var gulp = require('gulp');
 
-gulp.task('hello', function () {
-  console.log('您好');
+gulp.task('copy-index', function () {
+  return gulp.src('index.html').pipe(gulp.dest('dist'));
 });
-
-gulp.task('default', ['hello']);
